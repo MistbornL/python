1af05228b4c76d5014cb03005e8878ef43fda781
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-02-23 14:44:30 +0400

    Delete DNS_pointer_write_in_file directory

--- DNS_pointer_write_in_file/main.py
+++
@@ -1,8 +0,0 @@
-from services.functions import get_domain_ip, get_domain_name, writer
-
-if __name__ == "__main__":
-    hostnames, ip_adresses = [], []
-    get_domain_name(["8.8.8.8", "13.251.106.90"], hostnames)
-    writer("names.txt", "w", hostnames)
-    get_domain_ip(["dns.google", "ec2-13-251-106-90.ap-southeast-1.compute.amazonaws.com"], ip_adresses)
-    writer("ips.txt", "w", ip_adresses)
