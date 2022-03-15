1d0bfa70a49d6475f17a897359f33f039dab5329
MistbornL <lashaloria3@gmail.com>
2022-03-15 13:49:49 +0400

    guitar

---
+++ Guitar/main.py
@@ -0,0 +1,10 @@
+# {string : {fret:note}
+notes = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab']
+strings = {6:'E', 5:'A', 4:'D', 3:'G', 2:'B', 1:'E'}
+def main():
+    def string_fret(st, fr, note):
+
+
+
+if __name__ == "__main__":
+    main()
