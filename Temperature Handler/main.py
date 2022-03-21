9e9417170b5ee18ecff2e66dbf804208df82b41b
MistbornL <lashaloria3@gmail.com>
2022-03-21 15:08:18 +0400

    temp

--- Temperature Handler/main.py
+++ Temperature Handler/main.py
@@ -14,3 +14,19 @@ def converter(option, target):
         return round(temp - 273.5, 1)
     elif current == "kelvin" and target == "farenheit":
         return round((temp - 273.15) * 9 / 5 + 32, 1, 1)
+
+
+while True:
+    try:
+        current = input("Enter current temperature type: ")
+        temp  = int(input("Enter temperature: "))
+        target = input("Enter target to convert: ")
+
+        if current or temp or target != "fahrenheit" or "celcius" or "kelvin":
+            print("Enter valid temperature type!!")
+            continue
+    except ValueError:
+        print("invalid input")
+        continue
+
+print(converter([current, temp], target))
